cda080a6591d6c7d5ae6f1d0993acced32d55635
jhs512 <jangka512@gmail.com>
2021-12-15 21:23:48 +0900

    article 을 관리자에서 관리

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from article.models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['id', 'create_date', 'author', 'title']
+
+admin.site.register(Article, ArticleAdmin)
